8f12a4675af9a2995fede8f3f05f0177e752a710
Kender <kenderromain@gmail.com>
2023-05-30 17:07:06 -0400

    Start working on Category Views.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,26 @@ class CategoriesController < ApplicationController
     @categorie = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user = current_user
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :categories, only: [:new, :create]
+  resources :categories, only: [:index, :new, :create]
   # Defines the root path route ("/")
   root "categories#index"
 end
