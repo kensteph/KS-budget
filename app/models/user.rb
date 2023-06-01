9ad5a56d973e313e4e357e8bcf3c88abfafcb8c1
Kender <kenderromain@gmail.com>
2023-06-01 14:05:44 -0400

    Update Gemfilelock

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
   has_many :categories, foreign_key: 'user_id'
   has_many :user_transactions, foreign_key: :author_id
